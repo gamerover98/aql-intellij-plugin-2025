8f7e3d79202b3eb6e0efec827e77d0a1ac1735fc
gamerover98 <gamerover98@hotmail.it>
2025-06-12 19:56:22 +0200

    Refactor AqlConst to use constants from Constants.kt

---
+++ src/main/kotlin/com/arangodb/intellij/aql/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.arangodb.intellij.aql.util
+
+const val AQL_FILE_EXTENSION = "aql"
+const val AQL_FILE_NAME = "AQL file"
+const val AQL_FILE_DESCRIPTION = "AQL language file"
+const val AQL_LANGUAGE_ID = "AQL"
+const val TITLE_AQL_QUERY_PARAMETERS = "AQL Query Parameters"
